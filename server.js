324d7279fb381f693373fb295ba8c9d3e9492d1d
AlexsandroProjetosOficial <alexsandrofpf2011@gmail.com>
2020-02-28 10:38:01 -0300

    ajustando para deploy teste

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 //iniciando o DB
-mongoose.connect(`mongodb+srv://${process.env.MONGO_URL}`,{
+mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     serverSelectionTimeoutMS: 2000
@@ -24,9 +24,9 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_URL}`,{
 
 requireDir("./src/models");
 
-app.use('/rota',require('./src/routes'));
+app.use(require('./src/routes'));
 
 //iniciando o servidor
 //const port = process.env.PORT || 3001;
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3001);
