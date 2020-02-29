3856eca65647922163201f32a44235e99901838c
ThiagoAnd <thiago.1301801@fapi-pinhais.edu.br>
2020-02-29 02:28:20 -0300

    FEAT: Logica email-usuario
    Acrescentado logica para validação de email simples

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,34 +1,57 @@
 const mongoose = require("mongoose");
-const Usuario= mongoose.model("Usuario");
+const validator = require("email-validator");
+const Usuario = mongoose.model("Usuario");
 
 module.exports = {
-async showUsers(req,res){
-    const usuarios = await Usuario.find();
-    return res.json(usuarios);
-},
+    async showUsers(req, res) {
+        const usuarios = await Usuario.find();
+        return res.json(usuarios);
+    },
 
-async show(req,res){
-const usuario = await Usuario.findById(req.params.id);
-return res.json(usuario);
-},
+    async show(req, res) {
+        const usuario = await Usuario.findById(req.params.id);
+        return res.json(usuario);
+    },
 
-async createUser(req,res){
-const usuario = await Usuario.create(req.body);
-return res.json({
-    status: 200,
-    menssagem: 'Cadastrado com sucesso',
-    usuario
-    });
-},
+    async createUser(req, res) {
 
-async update(req,res){
-    const usuario = await Usuario.findByIdAndUpdate(req.params.id,req.body,{new: true, useFindAndModify: false});
-    return res.json(usuario); 
-},
+        const { nome, sobrenome, email, senha } = req.body;
+        //@ e . depois somente
+        if (!validator.validate(email)) {
+            return res.json({
+                status: 406,
+                menssagem: 'Formato do email não é valido',
+            })
 
-async destroy(req,res){
-    await Usuario.findByIdAndRemove(req.params.id);
-    return res.send();
-}
+        }
+        if(await Usuario.findOne({email})){
+            return res.json({
+                status: 412,
+                menssagem: 'Usuario ja existe',
+            })
+        }
+            
+        try {
+            const usuario = await Usuario.create(req.body);
+            return res.json({
+                status: 200,
+                menssagem: 'Cadastrado com sucesso',
+                usuario
+            });
+        } catch (err) {
+            return res.status(500).send({error:" Erro no registro do usuario" });
+        }
+
+    },
+
+    async update(req, res) {
+        const usuario = await Usuario.findByIdAndUpdate(req.params.id, req.body, { new: true, useFindAndModify: false });
+        return res.json(usuario);
+    },
+
+    async destroy(req, res) {
+        await Usuario.findByIdAndRemove(req.params.id);
+        return res.send();
+    }
 
 };
