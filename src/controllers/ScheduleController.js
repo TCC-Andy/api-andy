2d69d350225571987bb0482cda269be156b35bd8
ThiagoAnd <thiago.1301801@fapi-pinhais.edu.br>
2020-05-07 08:20:18 -0300

    FEAT: Funcionario -> Agenda
    DONE:
    
    # Logica para mostrar data agendada continuada.Retorno por enquanto os funcionarios de determinado servico requisitado
    
    # Correcao da variavel mensagem que estava errada
    
    TO DO:
    
    # Verificar se cada funcionario tem um registro de horario para determinado dia
    
    Coded

--- src/controllers/ScheduleController.js
+++ src/controllers/ScheduleController.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Agenda = mongoose.model("Agendas");
+const Funcionario = mongoose.model("Funcionario");
+
 
 module.exports = {
 
@@ -7,7 +9,7 @@ module.exports = {
     async createSchedule(req, res) {
 
         try {
-            
+
             //Criar os bloqueios aqui
 
             const sched = await Agenda.create(req.body);
@@ -19,7 +21,7 @@ module.exports = {
             })
 
         } catch (err) {
-            
+
             return res.json({
                 status: 500,
                 mensagem: 'Erro no registro da agenda',
@@ -31,14 +33,26 @@ module.exports = {
 
     async showDataSchedule(req, res) {
         try {
-            const { dataAgenda } = req.body;
-            
-            const sched = await Agenda.find({ 'dataAgenda': dataAgenda });
+            const { dataAgenda, idEmpresa, idServico } = req.body;
+
+            const func = await Funcionario.find({ idEmpresa, idServicos: { $in: [idServico] } });
+            //console.log(func[1].nome)
+            if (Object.keys(func).length < 1) {
+                return res.json({
+                    status: 500,
+                    mensagem: 'Nenhum funcionario cadastrado para este servico'
+                });
+            }
+
+            //Verifiquei os funcionarios, agora tenho que ir na agenda com a data e com o id do funcionario acredito eu, e 
+            //verificar se tenho registro para aquela data e aquele ID, se nao vou criar, se sim eu volto a data
+
+            const sched = await Agenda.find({ dataAgenda });
 
             return res.json({
 
                 status: 200,
-                sched
+                func
             });
 
         } catch (err) {
